2f0676a1ae15692054f9b321389a3517f3c17cf8
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-10 12:36:09 -0600

    utilizando el metodo every() para reemplazar el statement break o continue que foreach no permite

--- 37.ciclo_for.js
+++ 37.ciclo_for.js
@@ -18,6 +18,7 @@ array.forEach(function(valor){
     console.log(`El valor es: ${valor}`)
 })
 
+
 //some() comprueba si algún elemento del array pasa una prueba (proporcionado como una función).
 array.some(function(index,value){
     console.log(index,value);
@@ -25,3 +26,20 @@ array.some(function(index,value){
     //Ya que el ciclo foreach no permite utilizar el statement break, podemos usar en su lugar el metodo some
     return index === 3; 
 })
+
+
+//every(). La función every se comporta igual que foreach() excepto que deja de recorrer el array cada vez que la funcion de la devolucion de llamada devuelve un valor falso
+array.every(function(valor){
+    if (valor > 3){
+        return false;
+    }
+
+    if (valor == 1) {
+        console.log('recorrido de un array con la funcion every()');
+    }
+
+    console.log(valor);
+    return true;
+})
+
+//La funcion every() retorna el valor false que es equivalente a un break y true es equivalente a continue
