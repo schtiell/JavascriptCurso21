bb8382eacce36ad8ac77d69d0cfa42471fc438ae
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-10 11:52:20 -0600

    se hicieron cambios en el archivo 38 para conocer los ciclos while y do while

---
+++ 38.while_do_while.js
@@ -0,0 +1,28 @@
+//Ciclo do while
+let k = 0;
+
+do {
+    k++;
+
+    if (k == 5){
+        break;
+    }
+
+    console.log(`El valor de k es: ${k}`);
+    
+} while (k <= 10);
+
+
+//Ciclo do while
+let l = 0;
+
+do {
+    l++;
+
+    if (l == 5){
+        continue;
+    }
+
+    console.log(`El valor de l es: ${l}`);
+    
+} while (l <= 10);
