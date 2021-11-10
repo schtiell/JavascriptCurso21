9fe86ff0d73cd9e0b5b0887556c3657833f46844
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-10 11:54:07 -0600

    mas cambios para while y do while usando statement continue y break

--- 38.while_do_while.js
+++ 38.while_do_while.js
@@ -1,3 +1,33 @@
+//Ciclo while
+let i = 0;
+while (i < 10) {
+    
+    i ++;
+
+    //Cuando i toma el valor de 5 no se ejecutan las lineas siguientes sino que continua con la siguiente iteración del ciclo while.
+    if (i === 5){
+        continue;
+    }
+
+    console.log(`El valor de i es: ${i}`);
+
+}
+
+let j = 0;
+
+while (j < 10) {
+
+    j++;
+
+    //Cuando j toma el valor 5 se ejecuta un break que interrumpe la continuidad del ciclo.
+    if (j == 5){
+        break;
+    }
+
+    console.log(`El valor de j es: ${j}`);
+}
+
+
 //Ciclo do while
 let k = 0;
 
