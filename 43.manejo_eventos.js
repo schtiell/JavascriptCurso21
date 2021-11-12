f55eec2d2135b33b80689084a6ae1c9f66ff77c0
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-11-11 22:57:03 -0600

    Mas eventos y pruebas

--- 43.manejo_eventos.js
+++ 43.manejo_eventos.js
@@ -29,3 +29,21 @@ function onBlur(){
 function onMouseOut(){
     console.log('El mouse entra y sale del textbox');
 }
+
+var objeto = document.getElementById('objEvento');
+
+function msjEnConsola(){
+    console.log('Utilizando eventos con id de html y js!!');
+    console.log('Se disparó el evento click');
+}
+
+//addEventListener() Registra un evento a un objeto en específico.
+objeto.addEventListener("click",msjEnConsola);
+
+function msjEnConsolaKeyboard() {
+    console.log('Se disparó un evento de tecaldo');
+    console.log(arguments);
+}
+
+let tecla = document.getElementById('objEventoTeclado');
+tecla.addEventListener("keypress",msjEnConsolaKeyboard);
